1d072b77c7f5902e760df0f1261cbe48b9fa95df
Sakshi Patil <sakshispatil1111@gmail.com>
2025-06-04 23:15:34 +0530

    backend for income management

---
+++ server/controllers/incomeController.js
@@ -0,0 +1,45 @@
+const User = require("../models/User.js");
+const Income = require("../models/Income.js");
+
+exports.addIncome = async (req, res) => {
+  const userId = req.user.id;
+  try {
+    const { icon, source, amount, date } = req.body;
+
+    if (!source || !amount || !date) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const newIncome = new Income({
+      userId,
+      icon,
+      source,
+      amount,
+      date: new Date(date),
+    });
+
+    await newIncome.save();
+    res.status(200).json(newIncome);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+exports.getAllIncome = async (req, res) => {
+  const userId = req.user.id;
+  try {
+    const income = await Income.find({ userId }).sort({ date: -1 });
+    res.json(income);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+exports.deleteIncome = async (req, res) => {
+  try {
+    await Income.findByIdAndDelete(req.params.id);
+    res.json({ message: "Income deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
+};
