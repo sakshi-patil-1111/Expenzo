e1da704bdc5e289838c07901881d6bd7257922c4
Tanish Mittal <tanishmittal2k6@gmail.com>
2025-06-04 20:38:22 +0530

    backend setup

---
+++ server/controllers/authController.js
@@ -0,0 +1,11 @@
+const jwt = require("jsonwebtoken");
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+};
+
+exports.registerUser = async (req, res) => {};
+
+exports.loginUser = async (req, res) => {};
+
+exports.getUserInfo = async (req, res) => {};
---
+++ server/routes/authRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const {
+  registerUser,
+  loginUser,
+  getUserInfo,
+} = require("../controllers/authController.js");
+
+const router = express.Router();
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/getUser", protect, getUserInfo);
+
+module.exports = router;
